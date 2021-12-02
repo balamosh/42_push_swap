01499d262899eea0a936225c3df003b00271cce5
Spuckler Otherys <sotherys@student.21-school.ru>
2021-12-02 11:52:04 +0300

    fix stack swap

--- srcs/main.c
+++ srcs/main.c
@@ -6,16 +6,31 @@
 /*   By: sotherys <sotherys@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 23:37:22 by sotherys          #+#    #+#             */
-/*   Updated: 2021/12/02 09:18:12 by sotherys         ###   ########.fr       */
+/*   Updated: 2021/12/02 11:35:43 by sotherys         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "stack.h"
+#include <stdio.h>
+
+void	ft_stack_print(t_stack *stack)
+{
+	t_snode	*elem;
+
+	elem = stack->head;
+	while (elem)
+	{
+		ft_putnbr_fd(elem->val, 1);
+		write(1, "\n", 1);
+		elem = elem->next;
+	}
+	write(1, "_\n", 2);
+}
 
 int	main(int ac, char **av)
 {
 	t_stack	a;
-	t_snode	*elem;
+	char	c;
 	size_t	i;
 
 	if (ac == 1)
@@ -24,12 +39,19 @@ int	main(int ac, char **av)
 	i = ac - 1;
 	while (i > 0)
 		ft_stack_push_front(&a, ft_stack_new(ft_atoi(av[i--])));
-	elem = a.head;
-	while (elem)
+	ft_stack_print(&a);
+	while (TRUE)
 	{
-		ft_putnbr_fd(elem->val, 1);
-		write(1, "\n", 1);
-		elem = elem->next;
+		scanf("%c\n", &c);
+		if (c == 's')
+			ft_stack_swap(&a);
+		else if (c == 'r')
+			ft_stack_rotate(&a);
+		else if (c == 'e')
+			ft_stack_rrotate(&a);
+		else if (c == 'q')
+			break ;
+		ft_stack_print(&a);
 	}
 	return (0);
 }
--- srcs/stack/swap.c
+++ srcs/stack/swap.c
@@ -6,7 +6,7 @@
 /*   By: sotherys <sotherys@student.21-school.ru>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 10:07:14 by sotherys          #+#    #+#             */
-/*   Updated: 2021/12/02 10:36:06 by sotherys         ###   ########.fr       */
+/*   Updated: 2021/12/02 11:51:23 by sotherys         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,14 @@ void	ft_stack_swap(t_stack *stack)
 {
 	t_snode	*new_head;
 
+	if (stack->size < 2)
+		return ;
+	new_head = stack->head->next;
+	stack->head->next = new_head->next;
+	stack->head->prev = new_head;
+	new_head->next = stack->head;
+	new_head->prev = NULL;
+	stack->head = new_head;
 	if (stack->size == 2)
-	{
-		stack->head->next = NULL;
-		stack->tail->prev = NULL;
-		new_head = stack->tail;
-		stack->tail = stack->head;
-		stack->head = new_head;
-	}
-	else if (stack->size > 2)
-	{
-		new_head = stack->head->next;
-		stack->head->next = new_head->next;
-		stack->head->prev = new_head;
-		new_head->next = stack->head;
-		new_head->prev = NULL;
-		stack->head = new_head;
-	}
+		stack->tail = new_head->next;
 }
